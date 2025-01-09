925946dd677393a7d9a4925e453d98bcbc9179fb
rpahel <92730433+rpahel@users.noreply.github.com>
2025-01-09 09:04:39 +0100

    Highest score in main menu

--- RpahelCocos2D/Classes/Scenes/MainMenuScene.cpp
+++ RpahelCocos2D/Classes/Scenes/MainMenuScene.cpp
@@ -19,6 +19,32 @@ bool MainMenuScene::init()
 	const auto visibleSize = Director::getInstance()->getVisibleSize();
 	const Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
+	// Highest Score
+	int high_score_signed = UserDefault::getInstance()->getIntegerForKey("HIGH_SCORE");
+	unsigned int high_score_unsigned = 0;
+	std::memcpy(&high_score_unsigned, &high_score_signed, sizeof(int));
+
+	if (high_score_unsigned != 0)
+	{
+		auto scoreLabel = Label::createWithTTF("Highest score : " + std::to_string(high_score_unsigned), "fonts/arial.ttf", 16);
+		if (scoreLabel == nullptr)
+		{
+			printf("Error while loading: 'fonts/arial.ttf'\n");
+		}
+		else
+		{
+			scoreLabel->setPosition(
+				Vec2(
+					origin.x + visibleSize.width / 2,
+					origin.y + visibleSize.height - scoreLabel->getContentSize().height));
+
+			scoreLabel->setColor({ 0, 0, 0 });
+
+			this->addChild(scoreLabel, 1);
+		}
+	}
+
+
 	// Title
 	auto label = Label::createWithTTF("Timber Clicker", "fonts/Naluka.ttf", 48);
 	if (label == nullptr)
